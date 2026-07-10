e43a04e717c382fbfd22667b7bd65cd628f975f1
wassannc <balaram@wassan.org>
2026-07-10 16:43:39 +0530

    Update column_names.py

--- column_names.py
+++ column_names.py
@@ -1,1 +1,13 @@
+BREEDFARM_COLUMNS = {
 
+    "Project": "Project",
+
+    "pd.district": "District",
+
+    "pd.block": "Block",
+
+    "pd.gp": "Gram Panchayat",
+
+    "pd.village": "Village"
+
+}
