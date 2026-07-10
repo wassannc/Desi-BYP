adc27c2511242493b65ac57d28f8480a789472de
wassannc <balaram@wassan.org>
2026-07-10 11:59:56 +0530

    Update breedfarm.py

--- pages/breedfarm.py
+++ pages/breedfarm.py
@@ -1,7 +1,20 @@
 import streamlit as st
+from odk import get_form_data
+from config import PROJECTS, FORMS
 
-def show_breed_farm():
+def show_breedfarm():
+
     st.header("🗂 MIS Data")
+
     st.subheader("Breed Farm Assessment")
 
-    st.info("Breed Farm data will be displayed here.")
+    project = "Survey"
+
+    df = get_form_data(
+        PROJECTS[project]["project_id"],
+        FORMS["breedfarm"]
+    )
+
+    st.dataframe(df)
+
+    st.write("Records :", len(df))
