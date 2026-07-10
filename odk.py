360e718d1e1a1e285fdc6fe25b0c58c8687b1fec
wassannc <balaram@wassan.org>
2026-07-10 12:33:53 +0530

    Update odk.py

--- odk.py
+++ odk.py
@@ -3,21 +3,25 @@ import pandas as pd
 from requests.auth import HTTPBasicAuth
 from config import ODK_URL, USERNAME, PASSWORD
 
-
 def get_form_data(project_id, form_id):
 
     url = f"{ODK_URL}/v1/projects/{project_id}/forms/{form_id}.svc/Submissions"
 
-    response = requests.get(
-        url,
-        auth=HTTPBasicAuth(USERNAME, PASSWORD),
-        headers={"Accept": "application/json"}
-    )
+    try:
+        response = requests.get(
+            url,
+            auth=HTTPBasicAuth(USERNAME, PASSWORD),
+            headers={"Accept": "application/json"}
+        )
 
-    response.raise_for_status()
+        response.raise_for_status()
 
-    data = response.json()["value"]
+        data = response.json()["value"]
 
-    df = pd.json_normalize(data)
+        df = pd.json_normalize(data)
 
-    return df
+        return df
+
+    except Exception as e:
+        print(e)
+        return pd.DataFrame()
