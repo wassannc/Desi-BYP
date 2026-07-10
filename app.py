955786d7d4ccc5c50edbd1036e9b9ed28a4e6477
wassannc <balaram@wassan.org>
2026-07-10 13:06:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import streamlit as st
+import pandas as pd
 from odk import get_form_data
+from utils import standardize_dataframe
 from config import PROJECTS, FORMS
 
 st.set_page_config(
@@ -28,13 +29,29 @@ elif menu == "🗂 MIS Data":
 
     st.header("Breed Farm Assessment")
 
-    st.write("Loading data...")
+    frames = []
 
-    df = get_form_data(
-        PROJECTS["Survey Form"]["project_id"],
-        FORMS["breedfarm"]["form_id"]
+    for project_name, project in PROJECTS.items():
+
+        df = get_form_data(
+            project["project_id"],
+            FORMS["breedfarm"]
+        )
+
+        df = standardize_dataframe(
+            df,
+            project_name=project_name,
+            district=project["district"]
+        )
+
+        frames.append(df)
+
+    master_df = pd.concat(
+        frames,
+        ignore_index=True,
+        sort=False
     )
 
-    st.write(f"Total Records : {len(df)}")
+    st.write(f"Total Records : {len(master_df)}")
 
-    st.dataframe(df)
+    st.dataframe(master_df)
