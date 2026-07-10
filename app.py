7af678ae72318faebc2a67b2dd5f78cdacdbc9d2
wassannc <balaram@wassan.org>
2026-07-10 12:36:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ elif menu == "🗂 MIS Data":
     st.write("Loading data...")
 
     df = get_form_data(
-        PROJECTS["Survey"]["project_id"],
+        PROJECTS["Survey Form"]["project_id"],
         FORMS["breedfarm"]
     )
 
