d963ca0c9198b10b74e2a8a6dbe0143032fd94bd
wassannc <balaram@wassan.org>
2026-07-10 17:15:19 +0530

    Update column_names.py

--- column_names.py
+++ column_names.py
@@ -13,10 +13,15 @@ BREEDFARM_COLUMNS = {
     "pd.farmer_name": "Farmer name",
     
     "pd.project": "Project",
+    
     "pd.Mobile_no": "Mobile no.",
+    
     "pd.Completed_in": "Constructed in",
     "pd.SHG": "SHG name",
+    
     "pd.Family_members": "Total family members",
+    
     "pd.BFE_extent_acr": "BFE extent.acrs",
+    
     "pd.Photo_with_birds": "Photo"
 }
