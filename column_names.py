3159c683362ed4cf382c513f97fc4645f95aca78
wassannc <balaram@wassan.org>
2026-07-15 11:03:57 +0530

    Update column_names.py

--- column_names.py
+++ column_names.py
@@ -22,7 +22,27 @@ BREEDFARM_COLUMNS = {
 
     "pd.BFE_extent_acr": "BFE-Extent-acres",
 
-    "pd.GPS.coordinates": "GPS Location"
+    "pd.GPS.coordinates": "GPS Location",
+
+    "pd.GPS.properties.accuracy": "GPS accuracy",
+
+    "pd.Photo_with_birds": "Photo with Birds",
+
+    "pd.Photo_Forage": "Forage photo",
+
+    "fs.birds_type": "Type of birds available",
+
+    "fs.Total_Hens": "Total Hens",
+
+    "fs.Hens_incubates": "Hens-incubates",
+
+    "fs.Hens_layers": "Laying Hens",
+
+    "fs.Hens_with_Chicks": "Hens with chicks",
+
+    "fs.Dry_hens": "Dry Hens"
+
+    
 
     
 }
