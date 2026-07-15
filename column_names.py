a5659a8bbfcfee9254cb72e6831c8d3589c2b22d
wassannc <balaram@wassan.org>
2026-07-15 11:27:24 +0530

    Update column_names.py

--- column_names.py
+++ column_names.py
@@ -42,7 +42,22 @@ BREEDFARM_COLUMNS = {
 
     "fs.Dry_hens": "Dry Hens"
 
-    
+    "fs.Total_Cocks": "Total cocks",
 
-    
+    "fs.Total_Growers": "Total growers",
+
+    "fs.Total_chicks": "Total chicks",
+
+    "ready_sell.ready_to_sell": "Available ready to sell",
+
+    "ready_sell.eggs": "Eggs available readily as on 30th June2026",
+
+    "ready_sell.chicks": "Chicks available to sell",
+
+    "ready_sell.growers": "Growers available to sell",
+
+    "ready_sell.hens": "Hens available to sell",
+
+    "ready_sell.cocks": "Cocks available to sell"
+
 }
